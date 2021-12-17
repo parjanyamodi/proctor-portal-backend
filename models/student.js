6b10db16a50df024951ed1f1ee14920b36a75b46
Jeevan1351 <vijayeshjeevan.cs19@bmsce.ac.in>
2021-12-18 00:24:37 +0530

    Added more functions for Student Model

--- models/student.js
+++ models/student.js
@@ -35,6 +35,61 @@ Student.findMarks = (sid, result) => {
             result(err, null)
             return
         }
+        if(res.length){
+            console.log("Student Marks found!", res[0])
+            console.log(find_marks.sql)
+            res[0].message = "Student Marks Found"
+            result(null, res[0])
+            return
+        }
+        else{
+            console.log("Student Marks not found!")
+            result({type : "not_found"}, null)
+        }
+    })
+}
+
+Student.findDetails = (sid, result) => {
+    var find_details = sql.query(`select * from details where sid="${sid}";`, (err, res)=> {
+        if(err)
+        {
+            console.log("There was some error", err)
+            result(err, null)
+            return
+        }
+        if(res.length){
+            console.log("Student Details found!", res[0])
+            console.log(find_details.sql)
+            res[0].message = "Student Details Found"
+            result(null, res[0])
+            return
+        }
+        else{
+            console.log("Student Details not found!")
+            result({type : "not_found"}, null)
+        }
+    })
+}
+
+Student.findProctor = (sid, result) => {
+    var find_proctor = sql.query(`select * from proctor where pid=(select proctor from student where sid="${sid}");`, (err, res)=> {
+        if(err)
+        {
+            console.log("There was some error", err)
+            result(err, null)
+            return
+        }
+        if(res.length){
+            console.log("Student Profile found!", res[0])
+            console.log(find_proctor.sql)
+            res[0].message = "Student Found"
+            result(null, res[0])
+            return
+        }
+        else{
+            console.log("Student not found!")
+            result({type : "not_found"}, null)
+        }
     })
 }
 
